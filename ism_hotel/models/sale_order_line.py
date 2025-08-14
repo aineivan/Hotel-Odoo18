5d30300ba024cc5fac64fbd494d5bd202e2a32c7
Ivan_Aine <129157234+aineivan@users.noreply.github.com>
2025-08-14 18:21:21 +0300

    Update sale_order_line.py

--- ism_hotel/models/sale_order_line.py
+++ ism_hotel/models/sale_order_line.py
@@ -1,32 +1,37 @@
 from odoo import models, fields, api
-from odoo.fields import Command
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
-    
+
     duration = fields.Integer(string="Duration", required=True, default=1)
-    
+
     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id', 'duration')
     def _compute_amount(self):
         for line in self:
-            # Calculate price including duration
-            duration = line.duration or 1.0
-            price = line.price_unit * line.product_uom_qty * duration
+            if line.display_type:
+                line.update({
+                    'price_subtotal': 0.0,
+                    'price_tax': 0.0,
+                    'price_total': 0.0,
+                })
+                continue
 
+            qty = line.product_uom_qty * line.duration
             taxes = line.tax_id.compute_all(
-                price,
-                currency=line.currency_id or line.order_id.currency_id,
-                quantity=1.0,
+                line.price_unit,
+                line.order_id.currency_id,
+                qty,
                 product=line.product_id,
                 partner=line.order_id.partner_id
             )
 
-            line.price_subtotal = taxes['total_excluded']
-            line.price_tax = sum(t.get('amount', 0.0) for t in taxes.get('taxes', []))
-            line.price_total = taxes['total_included']
+            line.update({
+                'price_tax': sum(t['amount'] for t in taxes['taxes']),
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
+            })
 
     def _prepare_invoice_line(self, **optional_values):
-        """Prepare invoice line values"""
         self.ensure_one()
         res = {
             'display_type': self.display_type or 'product',
@@ -38,11 +43,10 @@ class SaleOrderLine(models.Model):
             'discount': self.discount,
             'duration': self.duration,
             'price_unit': self.price_unit,
-            'tax_ids': [Command.set(self.tax_id.ids)],
-            'sale_line_ids': [Command.link(self.id)],
+            'tax_ids': [(6, 0, self.tax_id.ids)],
+            'sale_line_ids': [(6, 0, [self.id])],
             'is_downpayment': self.is_downpayment,
         }
-
         analytic_account_id = self.order_id.analytic_account_id.id
         if self.analytic_distribution and not self.display_type:
             res['analytic_distribution'] = self.analytic_distribution
@@ -52,7 +56,6 @@ class SaleOrderLine(models.Model):
                 res['analytic_distribution'][analytic_account_id] = res['analytic_distribution'].get(analytic_account_id, 0) + 100
             else:
                 res['analytic_distribution'] = {analytic_account_id: 100}
-
         if optional_values:
             res.update(optional_values)
         if self.display_type:
