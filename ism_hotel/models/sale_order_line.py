ad0bfbfe2d4e1b3186afa3366e73022615351ada
Ivan_Aine <129157234+aineivan@users.noreply.github.com>
2025-08-14 18:10:20 +0300

    Update sale_order_line.py

--- ism_hotel/models/sale_order_line.py
+++ ism_hotel/models/sale_order_line.py
@@ -1,42 +1,33 @@
 from odoo import models, fields, api, _
 from odoo.fields import Command
-from odoo.exceptions import ValidationError
-
-import datetime
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     
     duration = fields.Integer(string="Duration", required=True, default=1)
     
-    # override _compute_amount
     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id', 'duration')
     def _compute_amount(self):
         for line in self:
-            tax_base_line_dict = line._convert_to_tax_base_line_dict()
-            
-            # print('before compute taxes : ', tax_base_line_dict)
-            tax_base_line_dict['quantity'] *= line.duration
-            tax_results = self.env['account.tax']._compute_taxes([tax_base_line_dict])
-            # print('after compute taxes : ', tax_results)
-            
-            totals = list(tax_results['totals'].values())[0]
-            amount_untaxed = totals['amount_untaxed']
-            amount_tax = totals['amount_tax']
+            # Default duration
+            duration = line.duration or 1.0
 
-            line.update({
-                'price_subtotal': amount_untaxed,
-                'price_tax': amount_tax,
-                'price_total': amount_untaxed + amount_tax,
-            })
-        
-    # override _prepare_invoice_line
+            # Compute taxes using Odoo 18 API
+            price = line.price_unit * line.product_uom_qty * duration
+            taxes = line.tax_id.compute_all(
+                price,
+                currency=line.currency_id or line.order_id.currency_id,
+                quantity=1.0,
+                product=line.product_id,
+                partner=line.order_id.partner_id
+            )
+
+            line.price_subtotal = taxes['total_excluded']
+            line.price_tax = sum(t.get('amount', 0.0) for t in taxes.get('taxes', []))
+            line.price_total = taxes['total_included']
+
     def _prepare_invoice_line(self, **optional_values):
-        """Prepare the values to create the new invoice line for a sales order line.
-
-        :param optional_values: any parameter that should be added to the returned invoice line
-        :rtype: dict
-        """
+        """Prepare the values to create the invoice line for a sales order line."""
         self.ensure_one()
         res = {
             'display_type': self.display_type or 'product',
@@ -52,6 +43,7 @@ class SaleOrderLine(models.Model):
             'sale_line_ids': [Command.link(self.id)],
             'is_downpayment': self.is_downpayment,
         }
+
         analytic_account_id = self.order_id.analytic_account_id.id
         if self.analytic_distribution and not self.display_type:
             res['analytic_distribution'] = self.analytic_distribution
@@ -61,6 +53,7 @@ class SaleOrderLine(models.Model):
                 res['analytic_distribution'][analytic_account_id] = res['analytic_distribution'].get(analytic_account_id, 0) + 100
             else:
                 res['analytic_distribution'] = {analytic_account_id: 100}
+
         if optional_values:
             res.update(optional_values)
         if self.display_type:
