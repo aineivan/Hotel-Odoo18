bfb7353e88ef2ecce14eeb11027eed1852642666
Ivan_Aine <129157234+aineivan@users.noreply.github.com>
2025-08-14 18:20:18 +0300

    Update sale_order.py

--- ism_hotel/models/sale_order.py
+++ ism_hotel/models/sale_order.py
@@ -3,14 +3,8 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    hotel_book_history_ids = fields.One2many(
-        'hotel.book.history', 'sale_order_id', string="Hotel Book History"
-    )
-    hotel_book_history_count = fields.Integer(
-        string="Hotel Book History Count",
-        compute="_compute_hotel_book_history_count",
-        store=False
-    )
+    hotel_book_history_ids = fields.One2many('hotel.book.history', 'sale_order_id', string="Hotel Book History")
+    hotel_book_history_count = fields.Integer(string="Hotel Book History Count", compute="_compute_hotel_book_history_count")
 
     @api.depends('hotel_book_history_ids')
     def _compute_hotel_book_history_count(self):
@@ -23,50 +17,34 @@ class SaleOrder(models.Model):
         action['domain'] = [('sale_order_id', '=', self.id)]
         return action
 
-    @api.depends('order_line.price_subtotal', 'order_line.price_tax', 'order_line.price_total')
+    @api.depends('order_line.price_subtotal', 'order_line.price_tax')
     def _compute_amounts(self):
-        res = super(SaleOrder, self)._compute_amounts()
+        super(SaleOrder, self)._compute_amounts()
         for order in self:
-            amount_untaxed = sum(line.price_subtotal for line in order.order_line)
-            amount_tax = sum(line.price_tax for line in order.order_line)
+            untaxed = sum(line.price_subtotal for line in order.order_line if not line.display_type)
+            tax = sum(line.price_tax for line in order.order_line if not line.display_type)
             order.update({
-                'amount_untaxed': amount_untaxed,
-                'amount_tax': amount_tax,
-                'amount_total': amount_untaxed + amount_tax,
+                'amount_untaxed': untaxed,
+                'amount_tax': tax,
+                'amount_total': untaxed + tax,
             })
-        return res
 
-    @api.depends_context('lang')
-    @api.depends('order_line.tax_id', 'order_line.price_unit', 'order_line.discount',
-                 'order_line.product_uom_qty', 'order_line.duration')
+    @api.depends('order_line.tax_id', 'order_line.price_unit', 'order_line.duration')
     def _compute_tax_totals(self):
-        res = super(SaleOrder, self)._compute_tax_totals()
         for order in self:
-            order_lines = order.order_line.filtered(lambda l: not l.display_type)
-            if not order_lines:
-                continue
-
-            currency = order.currency_id or order.company_id.currency_id
-            tax_totals = {
-                'amount_untaxed': 0.0,
-                'amount_tax': 0.0,
-                'amount_total': 0.0,
-            }
-
-            for line in order_lines:
-                duration = line.duration or 1.0
-                price = line.price_unit * line.product_uom_qty * duration
+            tax_totals = {}
+            for line in order.order_line:
+                if line.display_type:
+                    continue
+                qty = line.product_uom_qty * line.duration
                 taxes = line.tax_id.compute_all(
-                    price,
-                    currency=currency,
-                    quantity=1.0,
+                    line.price_unit,
+                    order.currency_id,
+                    qty,
                     product=line.product_id,
                     partner=order.partner_id
                 )
-                tax_totals['amount_untaxed'] += taxes['total_excluded']
-                tax_totals['amount_tax'] += sum(t.get('amount', 0.0) for t in taxes.get('taxes', []))
-
-            tax_totals['amount_total'] = tax_totals['amount_untaxed'] + tax_totals['amount_tax']
+                for t in taxes['taxes']:
+                    name = t['name']
+                    tax_totals[name] = tax_totals.get(name, 0.0) + t['amount']
             order.tax_totals = tax_totals
-
-        return res
