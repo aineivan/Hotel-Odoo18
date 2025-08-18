53e99579bdc35a007c48ee078b937f737266c9f2
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2025-08-18 17:34:09 +0300

    update hotel module

--- hotel_management_odoo/models/room_booking_line.py
+++ hotel_management_odoo/models/room_booking_line.py
@@ -9,15 +9,6 @@
 #    You can modify it under the terms of the GNU LESSER
 #    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
-#
-#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
-#    (LGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
 ###############################################################################
 from odoo import api, fields, models, tools, _
 from odoo.exceptions import ValidationError
@@ -33,101 +24,150 @@ class RoomBookingLine(models.Model):
     def _set_default_uom_id(self):
         return self.env.ref('uom.product_uom_day')
 
-    booking_id = fields.Many2one("room.booking", string="Booking",
-                                 help="Indicates the Room",
-                                 ondelete="cascade")
-    checkin_date = fields.Datetime(string="Check In",
-                                   help="You can choose the date,"
-                                        " Otherwise sets to current Date",
-                                   required=True)
-    checkout_date = fields.Datetime(string="Check Out",
-                                    help="You can choose the date,"
-                                         " Otherwise sets to current Date",
-                                    required=True)
-    room_id = fields.Many2one('hotel.room', string="Room",
-                              help="Indicates the Room",
-                              required=True,
-                              domain="[('status', '=', 'available')]")
-    uom_qty = fields.Float(string="Duration",
-                           help="The quantity converted into the UoM used by "
-                                "the product", readonly=True)
-    uom_id = fields.Many2one('uom.uom',
-                             default=_set_default_uom_id,
-                             string="Unit of Measure",
-                             help="This will set the unit of measure used",
-                             readonly=True)
-    price_unit = fields.Float(related='room_id.list_price', string='Rent',
-                              digits='Product Price',
-                              help="The rent price of the selected room.")
-    tax_ids = fields.Many2many('account.tax',
-                               'hotel_room_order_line_taxes_rel',
-                               'room_id', 'tax_id',
-                               related='room_id.taxes_ids',
-                               string='Taxes',
-                               help="Default taxes used when selling the room."
-                               , domain=[('type_tax_use', '=', 'sale')])
-    currency_id = fields.Many2one(string='Currency',
-                                  related='booking_id.pricelist_id.currency_id'
-                                  , help='The currency used')
-    price_subtotal = fields.Float(string="Subtotal",
-                                  compute='_compute_price_subtotal',
-                                  help="Total Price excluding Tax",
-                                  store=True)
-    price_tax = fields.Float(string="Total Tax",
-                             compute='_compute_price_subtotal',
-                             help="Tax Amount",
-                             store=True)
-    price_total = fields.Float(string="Total",
-                               compute='_compute_price_subtotal',
-                               help="Total Price including Tax",
-                               store=True)
-    state = fields.Selection(related='booking_id.state',
-                             string="Order Status",
-                             help=" Status of the Order",
-                             copy=False)
-    booking_line_visible = fields.Boolean(default=False,
-                                          string="Booking Line Visible",
-                                          help="If True, then Booking Line "
-                                               "will be visible")
+    booking_id = fields.Many2one(
+        "room.booking",
+        string="Booking",
+        help="Indicates the Room",
+        ondelete="cascade"
+    )
+    checkin_date = fields.Datetime(
+        string="Check In",
+        required=True,
+        help="You can choose the date, otherwise sets to current Date"
+    )
+    checkout_date = fields.Datetime(
+        string="Check Out",
+        required=True,
+        help="You can choose the date, otherwise sets to current Date"
+    )
+    room_id = fields.Many2one(
+        'hotel.room',
+        string="Room",
+        required=True,
+        domain="[('status', '=', 'available')]",
+        help="Indicates the Room"
+    )
+    uom_qty = fields.Float(
+        string="Duration",
+        readonly=True,
+        help="The quantity converted into the UoM used by the product"
+    )
+    uom_id = fields.Many2one(
+        'uom.uom',
+        default=_set_default_uom_id,
+        string="Unit of Measure",
+        readonly=True,
+        help="This will set the unit of measure used"
+    )
+    price_unit = fields.Float(
+        related='room_id.list_price',
+        string='Rent',
+        digits='Product Price',
+        help="The rent price of the selected room."
+    )
+    tax_ids = fields.Many2many(
+        'account.tax',
+        'hotel_room_order_line_taxes_rel',
+        'room_id', 'tax_id',
+        related='room_id.taxes_ids',
+        string='Taxes',
+        domain=[('type_tax_use', '=', 'sale')],
+        help="Default taxes used when selling the room."
+    )
+    currency_id = fields.Many2one(
+        string='Currency',
+        related='booking_id.pricelist_id.currency_id',
+        help='The currency used'
+    )
+    price_subtotal = fields.Float(
+        string="Subtotal",
+        compute='_compute_price_subtotal',
+        store=True,
+        help="Total Price excluding Tax"
+    )
+    price_tax = fields.Float(
+        string="Total Tax",
+        compute='_compute_price_subtotal',
+        store=True,
+        help="Tax Amount"
+    )
+    price_total = fields.Float(
+        string="Total",
+        compute='_compute_price_subtotal',
+        store=True,
+        help="Total Price including Tax"
+    )
+    state = fields.Selection(
+        related='booking_id.state',
+        string="Order Status",
+        copy=False,
+        help="Status of the Order"
+    )
+    booking_line_visible = fields.Boolean(
+        default=False,
+        string="Booking Line Visible",
+        help="If True, then Booking Line will be visible"
+    )
 
-    @api.onchange("checkin_date", "checkout_date")
+    @api.onchange("checkin_date", "checkout_date", "room_id")
     def _onchange_checkin_date(self):
-        """When you change checkin_date or checkout_date it will check
-        and update the qty of hotel service line
-        -----------------------------------------------------------------
-        @param self: object pointer"""
+        """Validate booking dates, update duration, and check room availability"""
+        if not self.checkin_date or not self.checkout_date:
+            return
+
+        # Validate dates
         if self.checkout_date < self.checkin_date:
             raise ValidationError(
-                _("Checkout must be greater or equal checkin date"))
-        if self.checkin_date and self.checkout_date:
-            diffdate = self.checkout_date - self.checkin_date
-            qty = diffdate.days
-            if diffdate.total_seconds() > 0:
-                qty = qty + 1
-            self.uom_qty = qty
+                _("Checkout must be greater or equal to checkin date")
+            )
+
+        # Compute duration in days
+        diffdate = self.checkout_date - self.checkin_date
+        qty = diffdate.days
+        if diffdate.total_seconds() > 0:
+            qty = qty + 1
+        self.uom_qty = qty
+
+        # Validate room availability
+        if self.room_id:
+            existing_bookings = self.env['room.booking.line'].search([
+                ('room_id', '=', self.room_id.id),
+                ('booking_id.state', 'in', ['reserved', 'check_in']),
+                ('id', '!=', self.id if self.id else 0)
+            ])
+            for existing_line in existing_bookings:
+                if (self.checkin_date < existing_line.checkout_date and
+                        self.checkout_date > existing_line.checkin_date):
+                    raise ValidationError(
+                        _("Room '%s' is already booked from %s to %s. "
+                          "Please choose different dates or another room.") % (
+                              self.room_id.name,
+                              existing_line.checkin_date.strftime(
+                                  '%Y-%m-%d %H:%M'),
+                              existing_line.checkout_date.strftime(
+                                  '%Y-%m-%d %H:%M')
+                        ))
 
     @api.depends('uom_qty', 'price_unit', 'tax_ids')
     def _compute_price_subtotal(self):
         """Compute the amounts of the room booking line."""
         for line in self:
             base_line = line._prepare_base_line_for_taxes_computation()
-            self.env['account.tax']._add_tax_details_in_base_line(base_line, self.env.company)
+            self.env['account.tax']._add_tax_details_in_base_line(
+                base_line, self.env.company
+            )
             line.price_subtotal = base_line['tax_details']['raw_total_excluded_currency']
-            print("total_excluded_currency",line.price_subtotal)
             line.price_total = base_line['tax_details']['raw_total_included_currency']
-            print("total_included_currency",line.price_total)
             line.price_tax = line.price_total - line.price_subtotal
-            if self.env.context.get('import_file',
-                                    False) and not self.env.user. \
-                    user_has_groups('account.group_account_manager'):
+            if (self.env.context.get('import_file', False) and
+                    not self.env.user.user_has_groups('account.group_account_manager')):
                 line.tax_id.invalidate_recordset(
-                    ['invoice_repartition_line_ids'])
+                    ['invoice_repartition_line_ids']
+                )
 
     def _prepare_base_line_for_taxes_computation(self):
-        """ Convert the current record to a dictionary in order to use the generic taxes computation method
-        defined on account.tax.
-
-        :return: A python dictionary.
+        """Convert the current record to a dictionary in order to use the
+        generic taxes computation method defined on account.tax.
         """
         self.ensure_one()
         return self.env['account.tax']._prepare_base_line_for_taxes_computation(
@@ -139,29 +179,3 @@ class RoomBookingLine(models.Model):
                 'currency_id': self.currency_id,
             },
         )
-
-
-    @api.onchange('checkin_date', 'checkout_date', 'room_id')
-    def onchange_checkin_date(self):
-        """Validate room availability for the given dates"""
-        if not (self.checkin_date and self.checkout_date and self.room_id):
-            return
-
-        # Search for existing bookings for the same room
-        existing_bookings = self.env['room.booking.line'].search([
-            ('room_id', '=', self.room_id.id),
-            ('booking_id.state', 'in', ['reserved', 'check_in']),
-            ('id', '!=', self.id if self.id else 0) 
-        ])
-
-        for existing_line in existing_bookings:
-            # Check if dates overlap
-            if (self.checkin_date < existing_line.checkout_date and
-                    self.checkout_date > existing_line.checkin_date):
-                raise ValidationError(
-                    _("Room '%s' is already booked from %s to %s. "
-                    "Please choose different dates or another room.") % (
-                        self.room_id.name,
-                        existing_line.checkin_date.strftime('%Y-%m-%d %H:%M'),
-                        existing_line.checkout_date.strftime('%Y-%m-%d %H:%M')
-                    ))
